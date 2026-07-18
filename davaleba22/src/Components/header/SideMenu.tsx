39f30abd847d875ba869cb3adb9d48cc41649c54
Archva19 <niniarchva@yahoo.com>
2026-07-18 19:42:39 +0400

    corrected css mistake

--- davaleba22/src/Components/header/SideMenu.tsx
+++ davaleba22/src/Components/header/SideMenu.tsx
@@ -16,19 +16,19 @@ export default function SideMenu({ menuVis, closeFun }:SideMenuProps) {
 
   return (
     <>
-      <div className="flex flex-col fixed top-0 bottom-0 right-0 left-0 w-full h-full bg-[#101010] pt-10 px-12 gap-5 text-[#B1B1B1] text-[18px] hover:text-[white]">
+      <div className="flex flex-col fixed top-0 bottom-0 right-0 left-0 w-full h-full bg-[#101010] pt-10 px-12 gap-5 text-[#B1B1B1] text-[18px]">
         <button onClick={() => closeFun()} className="w-6 cursor-pointer">
           <img src={closeIcon} alt="" />
         </button>
-        <a className="flex items-center justify-between cursor-pointer">
+        <a className="flex items-center justify-between cursor-pointer hover:text-[white]">
           <p>Jogos</p>
           <div className="flex gap-4 items-center text-[white]">
             <p>View All</p>
             <img src={rightArrow} alt="" />
           </div>
         </a>
-        <a className = "cursor-pointer">Loja</a>
-        <a className = "cursor-pointer">Ajuda</a>
+        <a className = "cursor-pointer hover:text-[white]">Loja</a>
+        <a className = "cursor-pointer hover:text-[white]">Ajuda</a>
       </div>
     </>
   );
